f9f5a680dbceda12a498d42ee07f651bba4edb85
Thomas Castleman <thomas7rex@gmail.com>
2019-05-11 23:11:10 -0400

    Finished basic admin portal

--- routes.js
+++ routes.js
@@ -46,8 +46,109 @@ module.exports = {
 
 		// admin portal
 		app.get('/admin', auth.isAdminGET, (req, res) => {
+			var render = {};
 
+			// get TA info
+			db.getTAs(function(err, TAs) {
+				if (!err) {
+					// store TAs in render object
+					render.TAs = TAs;
+				}
+
+				// get letter day info
+				db.getLetterDays(function(err, letterDays) {
+					if (!err) {
+						// store letter days in render object
+						render.letterDays = letterDays;
+					}
+
+					db.getAdmins(function(err, admins) {
+						if (!err) {
+							// store admin profiles info in render object
+							render.admins = admins;
+						}
+
+						// render admin tools page
+						res.render('admin.html', render);
+					});
+				});
+			});
 		});
+
+		// create a new TA user profile
+		app.post('/newTA', auth.isAdminPOST, (req, res) => {
+			// add new TA
+			db.addTA(req.body.name, req.body.phone, req.body.email, req.body.type, function(err) {
+				if (!err) {
+					// on success, redirect to admin portal
+					res.redirect('/admin');
+				} else {
+					res.render('error.html', {
+						message: "Failed to add a new TA user: " + err
+					});
+				}
+			});
+		});
+
+		// endpoint for removing existing TA users
+		app.post('/deleteTA', auth.isAdminPOST, (req, res) => {
+			// remove TA from DB
+			db.deleteTA(req.body.uid, function(err) {
+				res.send({ err: err });
+			});
+		});
+
+		// create a new letter day option
+		app.post('/newLetterDay', auth.isAdminPOST, (req, res) => {
+			// add letter day
+			db.addLetterDay(req.body.letter, function(err) {
+				if (!err) {
+					// on succes, redirect to admin portal
+					res.redirect('/admin');
+				} else {
+					res.render('error.html', {
+						message: "Failed to add new letter day: " + err
+					});
+				}
+			});
+		});
+
+		// endpoint for removing existing letter days
+		app.post('/deleteLetterDay', auth.isAdminPOST, (req, res) => {
+			// remove letter from DB
+			db.deleteLetterDay(req.body.uid, function(err) {
+				res.send({ err: err });
+			});
+		});
+
+		// create new system administrator
+		app.post('/newAdmin', auth.isAdminPOST, (req, res) => {
+			// add admin profile
+			db.addAdmin(req.body.name, req.body.email, function(err) {
+				if (!err) {
+					// on success, redirect to admin portal
+					res.redirect('/admin');
+				} else {
+					res.render('error.html', {
+						message: "Failed to add new admin: " + err
+					});
+				}
+			});
+		});
+
+		// endpoint for removing existing administrators
+		app.post('/deleteAdmin', auth.isAdminPOST, (req, res) => {
+			// check that admin is not deleting themself
+			if (req.body.uid != req.user.local.uid) {
+				// remove admin from DB
+				db.deleteAdmin(req.body.uid, function(err) {
+					res.send({ err: err });
+				});
+			} else {
+				res.send({ err: "You are unable to deauthorize yourself as an administrator." });
+			}
+		});
+
 
 		return module.exports;
 	}
