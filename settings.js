f5b11306e2501209586990b7c73dbd8158eae3e0
Thomas Castleman <thomas7rex@gmail.com>
2019-08-06 16:07:15 -0400

    Update settings.js with better test notification times

--- settings.js
+++ settings.js
@@ -18,14 +18,14 @@ module.exports = {
 	// email notifications schedule for a specific time of day
 	STATIC_EMAIL_NOTS: [
 		moment.utc('08:00:00', 'HH:mm:ss'),
-		moment.utc('21:10:20', 'HH:mm:ss')
+		moment.utc('12:00:00', 'HH:mm:ss')
 	],
 
 	// SMS notifications scheduled for a specific time of day
 	STATIC_SMS_NOTS: [
 		moment.utc('08:45:00', 'HH:mm:ss'),
 		moment.utc('10:00:00', 'HH:mm:ss'),
-		moment.utc('21:10:25', 'HH:mm:ss'),
+		moment.utc('12:00:00', 'HH:mm:ss'),
 	],
 
 	// email notifications occurring some duration BEFORE X Block
@@ -36,6 +36,7 @@ module.exports = {
 
 	// SMS notifications occurring some duration BEFORE X Block 
 	PRE_X_SMS_NOTS: [
+		moment.duration(30, 'minutes'),
 		moment.duration(5, 'minutes'),
 		moment.duration(0, 'minutes')
 	]
